397af2d863a5b289ff0cc1ab7cc66b58db0de7d4
Pawel Giczewski <>
2020-12-20 15:00:52 +0100

    make use of common steps

--- cypress/integration/Test/Test.ts
+++ cypress/integration/Test/Test.ts
@@ -1,9 +1,5 @@
 import { Given, Then } from "cypress-cucumber-preprocessor/steps";
 
-Given(/Open site/, () => {
-    cy.visit('cypress/fixtures/test.html')
-});
-
 Then(/Click link/, () => {
     cy.clickLink('click me')
 });
